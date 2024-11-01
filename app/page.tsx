ce0e71f6f749910b620447532459047202d5b118
Arnaud Delante <contact@arnaud.ninja>
2024-11-01 11:22:50 +0100

    Add Docker config

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import History from "@/components/history";
 import { deploysFormatter } from "@/lib/utils";
 
+export const dynamic = "force-dynamic";
+
 const Home = async () => {
   const deploys = await fetch(`${process.env.SUPABASE_BASE_URL}/Deploy`, {
     headers: {
-      apikey: process.env.SUPABASE_ANON_KEY ?? '',
+      apikey: process.env.SUPABASE_ANON_KEY ?? "",
       Authorization: `Bearer ${process.env.SUPABASE_ANON_KEY}`,
     },
   }).then((res) => res.json());
