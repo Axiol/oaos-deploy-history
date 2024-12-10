960ae56eb47b35c31701c3257f135478e31156e8
Arnaud Delante <contact@arnaud.ninja>
2024-12-10 23:46:02 +0100

    Add node scripts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  env: {
+    SUPABASE_BASE_URL: process.env.SUPABASE_BASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  },
 };
 
 export default nextConfig;
